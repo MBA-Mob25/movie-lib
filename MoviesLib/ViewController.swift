132edf9ec79952896a25323aab970580228f7837
Marcelo Mussi <mussi@prodev.com.br>
2022-08-30 19:17:48 -0300

    Initial Commit

---
+++ MoviesLib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Marcelo Mussi on 30/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
